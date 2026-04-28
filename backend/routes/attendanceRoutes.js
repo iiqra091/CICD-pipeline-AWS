51c22a73596b7694456a2857e9b34d9e0edf32db
iiqra091 <ibsheikh688@gmail.com>
2026-04-29 03:03:23 +0500

    Update

--- backend/routes/attendanceRoutes.js
+++ backend/routes/attendanceRoutes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { markAttendance, getAttendance } = require("../controllers/attendanceController");
 router.get("/", (req, res) => {
   res.json({ message: "Attendance route working" });
 });
