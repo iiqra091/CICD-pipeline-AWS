cc44a39cb8cc0e2f455693c008b93dfacccc6b0a
iiqra091 <ibsheikh688@gmail.com>
2026-04-29 00:42:35 +0500

    Update

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,20 @@ const app = express();
 
 connectDB();
 
+// BEFORE routes
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://13.60.56.26:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(cors({
-  origin: "http://http://13.60.56.26:3000",
+  origin: "http://13.60.56.26:3000",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
